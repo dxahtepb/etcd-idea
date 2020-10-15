f25c52689cac456c5cc6d6c2f3db9da71ccb228c
Andrey Chausov <dxahtepb@gmail.com>
2020-10-16 02:54:20 +0300

    Make toolwindow available while indexing

--- src/main/kotlin/com/github/dxahtepb/etcdidea/view/BrowserToolWindowFactory.kt
+++ src/main/kotlin/com/github/dxahtepb/etcdidea/view/BrowserToolWindowFactory.kt
@@ -1,12 +1,13 @@
 package com.github.dxahtepb.etcdidea.view
 
 import com.github.dxahtepb.etcdidea.service.EtcdService
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.content.ContentFactory
 
-class BrowserToolWindowFactory : ToolWindowFactory {
+class BrowserToolWindowFactory : ToolWindowFactory, DumbAware {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val myToolWindow = BrowserToolWindow(project, EtcdService.getInstance(project))
         val contentFactory = ContentFactory.SERVICE.getInstance()
