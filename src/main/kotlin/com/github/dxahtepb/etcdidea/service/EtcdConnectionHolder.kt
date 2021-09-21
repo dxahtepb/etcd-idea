2aa4cd654cde1cdf9d32055c499a14317871590a
Andrey Chausov <dxahtepb@gmail.com>
2021-09-21 04:20:39 +0300

    Refactor authentication process
    Make use of abandoned Authenticator architecture

--- src/main/kotlin/com/github/dxahtepb/etcdidea/service/EtcdConnectionHolder.kt
+++ src/main/kotlin/com/github/dxahtepb/etcdidea/service/EtcdConnectionHolder.kt
@@ -1,15 +1,9 @@
 package com.github.dxahtepb.etcdidea.service
 
 import com.github.dxahtepb.etcdidea.model.EtcdServerConfiguration
-import com.github.dxahtepb.etcdidea.service.auth.CredentialsService
-import com.github.dxahtepb.etcdidea.service.auth.PasswordKey
-import com.github.dxahtepb.etcdidea.toByteSequence
+import com.github.dxahtepb.etcdidea.service.auth.createAuthenticatorFor
 import com.intellij.openapi.diagnostic.logger
-import com.intellij.util.text.nullize
 import io.etcd.jetcd.Client
-import io.etcd.jetcd.ClientBuilder
-import io.grpc.netty.GrpcSslContexts
-import java.io.File
 
 private val LOG = logger<EtcdConnectionHolder>()
 
@@ -17,12 +11,15 @@ class EtcdConnectionHolder(configuration: EtcdServerConfiguration) : AutoCloseab
     private var client: Client
 
     init {
-        val builder = Client.builder()
-        builder.endpoints(configuration.hosts)
+        client = Client.builder()
+            .endpoints(configuration.hosts)
             .retryMaxDuration(configuration.timeouts.applicationTimeout)
             .keepaliveWithoutCalls(false)
-            .authUser(configuration)
-        client = builder.build()
+            .let { builder ->
+                val authenticator = createAuthenticatorFor(configuration)
+                authenticator.authenticate(builder)
+            }
+            .build()
         LOG.trace("Client $client built")
     }
 
@@ -33,26 +30,3 @@ class EtcdConnectionHolder(configuration: EtcdServerConfiguration) : AutoCloseab
         client.close()
     }
 }
-
-private fun ClientBuilder.authUser(configuration: EtcdServerConfiguration): ClientBuilder {
-    val sslConf = configuration.sslConfiguration
-    if (sslConf.sslEnabled) {
-        val sslContext = GrpcSslContexts
-            .forClient()
-            .trustManager(createFile(sslConf.rootCertificate))
-            .keyManager(createFile(sslConf.clientKeyChain), createFile(sslConf.clientKey))
-            .build()
-        this.sslContext(sslContext)
-    } else {
-        if (configuration.user.isNotEmpty()) {
-            this.user(configuration.user.toByteSequence())
-            val password = CredentialsService.instance.getPassword(PasswordKey(configuration.id))
-            if (password != null) {
-                this.password(password.toByteSequence())
-            }
-        }
-    }
-    return this
-}
-
-private fun createFile(path: String): File? = path.nullize()?.let { File(it) }
--- src/main/kotlin/com/github/dxahtepb/etcdidea/service/auth/EtcdAuthenticator.kt
+++ src/main/kotlin/com/github/dxahtepb/etcdidea/service/auth/EtcdAuthenticator.kt
@@ -1,11 +1,43 @@
 package com.github.dxahtepb.etcdidea.service.auth
 
+import com.github.dxahtepb.etcdidea.model.EtcdServerConfiguration
+import com.github.dxahtepb.etcdidea.model.EtcdSslConfiguration
+import com.github.dxahtepb.etcdidea.toByteSequence
+import com.intellij.util.text.nullize
+import io.etcd.jetcd.ClientBuilder
+import io.grpc.netty.GrpcSslContexts
+import java.io.File
+
 interface EtcdAuthenticator {
-    fun authenticate()
+    fun authenticate(builder: ClientBuilder): ClientBuilder
 }
 
-data class EtcdPasswordAuthenticator(val user: String, val password: String) : EtcdAuthenticator {
-    override fun authenticate() {
-        TODO("Not yet implemented")
+fun createAuthenticatorFor(configuration: EtcdServerConfiguration) =
+    if (configuration.sslConfiguration.sslEnabled) EtcdSslAuthenticator(configuration.sslConfiguration)
+    else EtcdPasswordAuthenticator(configuration.id, configuration.user)
+
+class EtcdPasswordAuthenticator(private val configurationId: String, private val user: String) : EtcdAuthenticator {
+    override fun authenticate(builder: ClientBuilder): ClientBuilder {
+        if (user.isNotEmpty()) {
+            builder.user(user.toByteSequence())
+            val password = CredentialsService.instance.getPassword(PasswordKey(configurationId))
+            if (password != null) {
+                builder.password(password.toByteSequence())
+            }
+        }
+        return builder
     }
+}
+
+class EtcdSslAuthenticator(private val sslConfiguration: EtcdSslConfiguration) : EtcdAuthenticator {
+    override fun authenticate(builder: ClientBuilder): ClientBuilder {
+        return GrpcSslContexts
+            .forClient()
+            .trustManager(createFile(sslConfiguration.rootCertificate))
+            .keyManager(createFile(sslConfiguration.clientKeyChain), createFile(sslConfiguration.clientKey))
+            .build()
+            .let { builder.sslContext(it) }
+    }
+
+    private fun createFile(path: String): File? = path.nullize()?.let { File(it) }
 }
